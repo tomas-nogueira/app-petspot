a85909153d1c3114f7c8a5b53c9011e10e7a345f
tomas-nogueira <tomasmnogueira2006@gmail.com>
2024-04-23 15:38:19 -0300

    Tela de busca usuário conectada com a API

--- src/Components/Busca.js
+++ src/Components/Busca.js
@@ -1,9 +1,135 @@
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TextInput, Image, ActivityIndicator } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function Busca() {
-  return(
-    <View>
-        <Text>Busca</Text>
+
+  const [usuarios, setUsuarios] = useState([]);
+  const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // Novo estado para indicar se está carregando
+
+  async function getUsuarios() {
+     // Indicar que a busca está em andamento
+    await fetch('https://fakestoreapi.com/users', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(res => (res.ok === true) ? res.json() : false)
+      .then(json => {
+        setUsuarios(json); // Indicar que a busca terminou
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+
+  useEffect(() => {
+    setFiltro(usuarios.filter((item) => item.name.firstname === busca)[0]);
+  }, [busca]);
+
+  return (
+    <View style={css.container}>
+      <View style={css.boximg}>
+        <Image source={require('../../assets/oracle.png')} style={css.img} />
+      </View>
+      <View style={css.boxsearch}>
+        <TextInput
+          style={css.input}
+          placeholder="Digite aqui para buscar..."
+          placeholderTextColor="#888"
+          value={busca}
+          onChangeText={(digitado) => setBusca(digitado)}
+        />
+        <FontAwesome name="search" size={24} color="#7E2C28" style={css.searchIcon} />
+      </View>
+      {isLoading ? (
+        // Mostrar ActivityIndicator enquanto a busca está em andamento
+        <ActivityIndicator style={css.loadingIndicator} size="large" color="#7E2C28" />
+      ) : (
+        filtro && (
+          <View style={css.userContainer}>
+            <View style={css.profileIcon}>
+              <FontAwesome name="user-circle-o" size={60} color="white" />
+            </View>
+            <View style={css.userBox}>
+              <Text style={[css.userText, css.boldText]}>Nome do usuário: {filtro.name.firstname} {filtro.name.lastname}</Text>
+              <Text style={[css.userText, css.boldText]}>E-mail: {filtro.email}</Text>
+              <Text style={[css.userText, css.boldText]}>Telefone: {filtro.phone}</Text>
+            </View>
+          </View>
+        )
+      )}
     </View>
   );
-}
+};
+
+const css = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111123',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  boxsearch: {
+    backgroundColor: '#494959',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    height: 60,
+    fontSize: 16,
+    color: 'white'
+  },
+  searchIcon: {
+    marginLeft: 10,
+  },
+  img: {
+    alignItems: 'center',
+    height: 210,
+    width: 220,
+    resizeMode: 'contain'
+  },
+  boximg: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 140
+  },
+  userContainer: {
+    backgroundColor: '#28283d',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  profileIcon: {
+    marginBottom: 20
+  },
+  userBox: {
+    alignItems: 'center'
+  },
+  userText: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 10
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  loadingIndicator: {
+    marginTop: 20 // Espaçamento acima do indicador
+  }
+});
